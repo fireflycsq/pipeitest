7d9779335f6a9c094651c72417f7d7e4086e0ed0
fireflycsq <fireflycsq@163.com>
2017-05-24 18:12:41 +0800

    Add files via upload

---
+++ pipeitest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#cherish
+
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
+
+import os
+# import re
+
+# input1 = open(os.getcwd()+r'\\test.txt')
+# output=open(os.getcwd()+r'\\out.txt','w')
+
+# for content in input1:
+# 	items=[]
+# 	print content
+# 	items = re.findall('status":{"created_at":.*?,"text":"+(.*?)","source":"<a href',content.decode("utf-8"),re.S)
+# 	if items:
+# 		output.write('%s\n'%(items[0]))
+# 		print items
+# 	else:
+# 		continue
+
+# output.close()
+# input1.close()
+
+# #返回上一级目录
+# print os.path.abspath(os.path.dirname(__file__)+os.path.sep+"..")  
